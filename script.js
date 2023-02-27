4bee46c2d2d70c84147dcc1595e923884bddde96
Adam Shalala <adamshalala@Adams-Air.localdomain>
2023-02-27 13:53:03 +1100

    user prompts and validation done

--- script.js
+++ script.js
@@ -1,28 +1,57 @@
+// 3  create password
+
+//  4 display password
+// return "this will be your password"
+
+function generatePassword() {
+  console.log("You've clicked the generate password button");
+
+  // 1. User Prompts for password criteria
+
+  // a)password length is number between 8 and 128
+
+  var userLength = +prompt("Character length of password: Min 8 Max 128 ");
+  if (userLength >= 8 && userLength <= 129) {
+    console.log("User Length selected ", userLength);
+  } else {
+    alert("Please only pick between 8 and 128 characters!");
+    return; // exit the function - valodates input
+  }
+
+  var upperCase = confirm("Include upper");
+  if (upperCase) {
+    console.log("user chose to use uppercase", upperCase);
+  } else {
+    console.log("user chose no uppar case letters", upperCase);
+  }
+
+  var lowerCase = confirm("Include lowercase letters?");
+  if (lowerCase) {
+    console.log("user chose to use lowercase", lowerCase);
+  } else {
+    console.log("user chose no lowercase letters", lowerCase);
+  }
+
+  var userNumbers = confirm("Use numbers");
+  if (userNumbers) {
+    console.log("user chose to use lowercase", userNumbers);
+  } else {
+    console.log("user chose no lowercase letters", userNumbers);
+  }
+  var userSymbols = confirm("Use symbols?");
+  if (userSymbols) {
+    console.log("user chose to use lowercase", userSymbols);
+  } else {
+    console.log("user chose no lowercase letters", userSymbols);
+  }
 
 
 
-function generatePassword () {
-console.log ("You've clicked the generate password button")
-
-// 1. User Prompts for password criteria
-
-// a)password length is nuber between 8 and 128
-
-// b)confirm, lower, upper, numbers, special characters
-
-// validate the input - at least one has to be selected if not. exit function 
-
-// create password
-
-// display password 
 
 
-return "this will be your password"
 
+  
 }
-
-
-
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -33,7 +62,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
