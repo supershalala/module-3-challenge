52f4dd3d078b4756e026a1752114a2fe8a0ae1f7
Adam Shalala <adamshalala@Adams-Air.localdomain>
2023-02-27 12:02:11 +1100

    pseudo coding on scipt js

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+
+
+
+function generatePassword () {
+console.log ("You've clicked the generate password button")
+
+// 1. User Prompts for password criteria
+
+// a)password length is nuber between 8 and 128
+
+// b)confirm, lower, upper, numbers, special characters
+
+// validate the input - at least one has to be selected if not. exit function 
+
+// create password
+
+// display password 
+
+
+return "this will be your password"
+
+}
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
